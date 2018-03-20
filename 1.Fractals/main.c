01ef0e9c405e4171b59fa8f757a7821a5ff16c61
MaanooAk <akritasak@gmail.com>
2018-03-20 14:53:47 +0200

    Fractals: Resize handling

--- 1.Fractals/main.c
+++ 1.Fractals/main.c
@@ -10,26 +10,30 @@ typedef GLfloat point2[2];
 
 int w, h;
 
-int n = 5;
+int n = 6;
 float r = 1.0f / 3.0f;
 int loops = 10000;
 char colorful = 0;
+int seed;
 
 float x_off = 0, y_off = 0;
 
-void myinit(void) {
+void init(void) {
 
-/* attributes */
 	glEnable(GL_BLEND);
 	glClearColor(1.0, 1.0, 1.0, 0.0); /* white background */
 	glColor3f(1.0, 0.0, 0.0); /* draw in red */
 
-/* set up viewing */
-/* 500 x 500 window with origin lower left */
+	set_projection();
+}
 
+void set_projection() {
+
+	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0.0 + x_off, w + x_off, 0.0 + y_off, h + y_off);
+
 	glMatrixMode(GL_MODELVIEW);
 }
 
@@ -39,8 +43,8 @@ void create_polygon(int n, point2 vertices[]) {
 
 	for (int i = 0; i < n; i++) {
 
-		vertices[i][0] = w/2 + sin(i * TAU / n) * size;
-		vertices[i][1] = h/2 + cos(i * TAU / n) * size;
+		vertices[i][0] = w / 2 + sin(i * TAU / n) * size;
+		vertices[i][1] = h / 2 + cos(i * TAU / n) * size;
 	}
 
 }
@@ -54,6 +58,8 @@ void set_random_color() {
 void display(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	srand(seed);
 
 	point2 vertices[n];
 	create_polygon(n, vertices);
@@ -70,8 +76,8 @@ void display(void) {
 
 		int j = rand() % n;
 
-		p[0] += (vertices[j][0] - p[0]) * (1-r);
-		p[1] += (vertices[j][1] - p[1]) * (1-r);
+		p[0] += (vertices[j][0] - p[0]) * (1 - r);
+		p[1] += (vertices[j][1] - p[1]) * (1 - r);
 
 		if (colorful) {
 			set_random_color();
@@ -108,25 +114,18 @@ void motion(int mouse_x, int mouse_y) {
 		mouse_last_x = mouse_x;
 		mouse_last_y = mouse_y;
 
-		myinit();
+		set_projection();
 		glutPostRedisplay();
-
 	}
-
-
 }
 
 void reshape(int new_w, int new_h) {
 
-//	w = new_w;
-//	h = new_h;
-//
-//	glMatrixMode(GL_PROJECTION);
-//	glLoadIdentity();
-//	gluOrtho2D(0.0, w, 0.0, h);
-//	glMatrixMode(GL_MODELVIEW);
-//
-//	glutPostRedisplay();
+	w = new_w;
+	h = new_h;
+
+	set_projection();
+	glutPostRedisplay();
 }
 
 void menu(int id) {
@@ -146,6 +145,7 @@ void menu(int id) {
 			break;
 	}
 
+	create_seed();
 	glutPostRedisplay();
 }
 
@@ -160,15 +160,22 @@ void create_menu() {
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
+void create_seed() {
+
+	// TODO make true random
+	seed = rand();
+}
+
 void main(int argc, char **argv) {
 
-/* Standard GLUT initialization */
+	w = 500;
+	h = 500;
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); /* default, not needed */
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
-	glutInitWindowSize(w = 500, h = 500); /* 500 x 500 pixel window */
-	glutInitWindowPosition(0, 0); /* place window top left on display */
+	glutInitWindowSize(w, h);
+	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Fractals");
 
 	glutDisplayFunc(display);
@@ -176,9 +183,10 @@ void main(int argc, char **argv) {
 	glutMotionFunc(motion);
 	glutReshapeFunc(reshape);
 
+	create_seed();
 	create_menu();
 
-	myinit();
+	init();
 
 	glutMainLoop(); /* enter event loop */
 }

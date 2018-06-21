2f583ca06038e51f8cc433acfa1ee53fe21bbcb3
MaanooAk <akritasak@gmail.com>
2018-06-21 17:06:30 +0300

    House: Reformated to code style

--- 3.House/main.c
+++ 3.House/main.c
@@ -14,7 +14,7 @@
 typedef GLfloat point3[3];
 
 enum options {
-    EXIT, FIRST_MODE, SECOND_MODE//, CUBE, ICOSAHEDRON
+	EXIT, FIRST_MODE, SECOND_MODE//, CUBE, ICOSAHEDRON
 };
 
 // === Data ===
@@ -67,185 +67,184 @@ void menu(int id);
 
 void init() {
 
-    glEnable(GL_BLEND);
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glColor3f(1.0, 0.0, 0.0);
+	glEnable(GL_BLEND);
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glColor3f(1.0, 0.0, 0.0);
 
-    set_projection();
+	set_projection();
 }
 
 void set_projection() {
 
-    int s = 50;
+	int s = 50;
 
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-s, s, -s, s, -200, 200);
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-s, s, -s, s, -200, 200);
 
-    glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void create_component() {
 
-    glNewList(1, GL_COMPILE);
-    {
-        glBegin(GL_POLYGON);
-        glVertex3f(-1, 1, 1);
-        glVertex3f(1, 1, 1);
-        glVertex3f(1, -1, 1);
-        glVertex3f(-1, -1, 1);
-        glEnd();
-    }
-    glEndList();
+	glNewList(1, GL_COMPILE);
+	{
+		glBegin(GL_POLYGON);
+		glVertex3f(-1, 1, 1);
+		glVertex3f(1, 1, 1);
+		glVertex3f(1, -1, 1);
+		glVertex3f(-1, -1, 1);
+		glEnd();
+	}
+	glEndList();
 }
 
 void draw_component(float rotate_x, float rotate_y, float size) {
-    glPushMatrix();
+	glPushMatrix();
 
-    float scale = size / 2; // 2 is the size of the component
-    glScalef(scale, scale, scale);
-    glRotatef(rotate_x, 1, 0, 0);
-    glRotatef(rotate_y, 0, 1, 0);
+	float scale = size / 2; // 2 is the size of the component
+	glScalef(scale, scale, scale);
+	glRotatef(rotate_x, 1, 0, 0);
+	glRotatef(rotate_y, 0, 1, 0);
 
-    set_random_color();
-    glCallList(1);
+	set_random_color();
+	glCallList(1);
 
-    glPopMatrix();
+	glPopMatrix();
 }
 
 void draw_shape() {
 
-    draw_cube();
+	draw_cube();
 }
 
 void draw_cube() {
-    glPushMatrix();
+	glPushMatrix();
 
-    draw_component(0, 0, param_a);
-    draw_component(0, 90, param_a);
-    draw_component(0, 180, param_a);
-    draw_component(0, 270, param_a);
-    draw_component(90, 0, param_a);
-    draw_component(-90, 0, param_a);
+	draw_component(0, 0, param_a);
+	draw_component(0, 90, param_a);
+	draw_component(0, 180, param_a);
+	draw_component(0, 270, param_a);
+	draw_component(90, 0, param_a);
+	draw_component(-90, 0, param_a);
 
-    glPopMatrix();
+	glPopMatrix();
 }
 
 void set_random_color() {
 
-    glColor3ub(rand() % 255, rand() % 255, rand() % 255);
+	glColor3ub(rand() % 255, rand() % 255, rand() % 255);
 }
 
 void display() {
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    srand(seed);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	srand(seed);
 
-    glPushMatrix();
+	glPushMatrix();
 
-    float angle = t * angle_speed; // in degrees
-    float scale = 1 + (1 + sin(t * scale_speed) * 1.0f); // from 1 to 3
+	float angle = t * angle_speed; // in degrees
+	float scale = 1 + (1 + sin(t * scale_speed) * 1.0f); // from 1 to 3
 
-    if (mode==FIRST_MODE) {
+	if (mode == FIRST_MODE) {
 
-        glTranslatef(0, 0, -param_b);
-        glRotatef(angle, param_v[0], param_v[1], param_v[2]);
-        glScalef(scale, scale, scale);
+		glTranslatef(0, 0, -param_b);
+		glRotatef(angle, param_v[0], param_v[1], param_v[2]);
+		glScalef(scale, scale, scale);
 
-        draw_shape();
-    }
-    else if(mode==SECOND_MODE){
-        glTranslated(0,0,-8*param_b/10);
-        glRotated(angle,param_v[0],param_v[1],param_v[2]);
-        glTranslated(0,0,8*param_b/10);
+		draw_shape();
+	} else if (mode == SECOND_MODE) {
+		glTranslated(0, 0, -8 * param_b / 10);
+		glRotated(angle, param_v[0], param_v[1], param_v[2]);
+		glTranslated(0, 0, 8 * param_b / 10);
 
-        glRotatef(angle, param_v[0], param_v[1], param_v[2]);
-        glScalef(scale, scale, scale);
+		glRotatef(angle, param_v[0], param_v[1], param_v[2]);
+		glScalef(scale, scale, scale);
 
-        draw_shape();
-    }
+		draw_shape();
+	}
 
-    glPopMatrix();
+	glPopMatrix();
 
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void idle() {
 
-    t = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+	t = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
 
-    glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 void reshape(int new_w, int new_h) {
 
-    w = new_w;
-    h = new_h;
+	w = new_w;
+	h = new_h;
 
-    set_projection();
-    glutPostRedisplay();
+	set_projection();
+	glutPostRedisplay();
 }
 
 void menu(int id) {
 
-    switch(id){
-        case EXIT:
-            exit(0);
-            break;
-        case FIRST_MODE:
-            mode=FIRST_MODE;
-            break;
-        case SECOND_MODE:
-            mode=SECOND_MODE;
-            break;
-        default:
-            return;
-    }
+	switch (id) {
+		case EXIT:
+			exit(0);
+			break;
+		case FIRST_MODE:
+			mode = FIRST_MODE;
+			break;
+		case SECOND_MODE:
+			mode = SECOND_MODE;
+			break;
+		default:
+			return;
+	}
 
-    glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 void create_menu() {
 
-    glutCreateMenu(menu);
+	glutCreateMenu(menu);
 
-    glutAddMenuEntry("First mode", FIRST_MODE);
-    glutAddMenuEntry("Second mode", SECOND_MODE);
-    glutAddMenuEntry("Exit", EXIT);
+	glutAddMenuEntry("First mode", FIRST_MODE);
+	glutAddMenuEntry("Second mode", SECOND_MODE);
+	glutAddMenuEntry("Exit", EXIT);
 
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 }
 
 void create_seed() {
 
-    //srand(time(0)); // risky random seed
-    srand(3); // nice fixed random seed
-    seed = rand();
+	//srand(time(0)); // risky random seed
+	srand(3); // nice fixed random seed
+	seed = rand();
 }
 
 void main(int argc, char **argv) {
 
-    w = 500;
-    h = 500;
+	w = 500;
+	h = 500;
 
-    glutInit(&argc, argv);
+	glutInit(&argc, argv);
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(w, h);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("House");
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(w, h);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("House");
 
-    glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
-    glutDisplayFunc(display);
-    glutIdleFunc(idle);
-    glutReshapeFunc(reshape);
-    create_seed();
-    create_menu();
-    create_component();
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);
+	create_seed();
+	create_menu();
+	create_component();
 
-    init();
-    glutMainLoop();
+	init();
+	glutMainLoop();
 }
